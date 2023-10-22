53bbbef5c43afae804a30c43f7aca0e64dac9ffa
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2023-10-22 20:41:22 +0700

    Challenge-5 Add Middleware in API

--- challange-5/index.js
+++ challange-5/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const AuthController = require('./App/Controllers/AuthController');
 const carController = require('./App/Controllers/CarController');
-const userController = require('./App/Controllers/UserController');
+const AuthMiddleware = require('./App/Middleware/AuthMiddleware');
 const findAndSetFeedById = require('./App/Middleware/CarMiddleware');
 
 const port = process.env.PORT || 8000;
@@ -8,12 +9,16 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/login/super-admin", userController.superAdminLogin)
-app.get("/cars", carController.index);
-app.post('/cars', carController.store);
-app.get("/cars/:id", findAndSetFeedById, carController.show);
-app.put("/cars/:id", carController.update);
-app.delete("/cars/:id", carController.delete);
+app.post("/login", AuthMiddleware.isUserHasRegister, AuthController.userLogin);
+app.post("/register",  AuthMiddleware.validateBodyRequest, AuthMiddleware.isUserHasNotRegister, AuthController.userRegister);
+
+app.get("/cars", AuthMiddleware.authorize, carController.index);
+app.get("/cars/:id", AuthMiddleware.authorize, findAndSetFeedById, carController.show);
+
+app.post('/cars', AuthMiddleware.authorize, AuthMiddleware.isSuperAdminAndAdmin, carController.store);
+app.delete("/cars/:id", AuthMiddleware.authorize, AuthMiddleware.isSuperAdminAndAdmin, carController.delete);
+app.put("/cars/:id", AuthMiddleware.authorize, AuthMiddleware.isSuperAdminAndAdmin, carController.update);
+
 app.get('*', (req, res) => {
     res.status('200').json({"message": "successfully"})
 })
